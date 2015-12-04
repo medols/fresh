ec2bd8987496e61353a3dfd0fc0563d92033fe5d
macbookpr02010 <github@gianduia.net>
2015-12-04 19:32:17 +0100

    added spec number 5

--- spec/fresh_spec.rb
+++ spec/fresh_spec.rb
@@ -229,5 +229,92 @@ describe "Fresh gather api" do
 
   end
 
+  it "broadcasts 0..6 values from vector individually to three computing nodes and gathers them for display after processing" do
+
+    lambda{
+
+      fresh [
+
+        proc{
+          7.times{|i|
+            coef=[1,2,1,2,1,2,3,2,3,2]
+            val=[0]
+            gr2=[1,2,3]
+            ch2=[0,0]
+            ms2=[0,0,0]
+            mpi_gather_recv gr2 , ms2 , ch2
+            val[0] = ms2[0]*coef[7] + ms2[1]*coef[8] + ms2[2]*coef[9]
+            puts val.to_s
+          }
+        },
+
+        proc{
+          7.times{|i|
+            coef=[1,2,1,2,1,2,3,2,3,2]
+            gr1=[4]
+            ch1=[0,0]
+            ms1=[0]
+            gr2=[0]
+            ms2=[0,10]
+            mpi_gather_recv gr1 , ms1 , ch1
+            ms2[1]=ms1[0]*coef[1]
+            mpi_gather_send gr2 , ms2
+          }
+        },
+
+        proc{
+          7.times{|i|
+            coef=[1,2,1,2,1,2,3,2,3,2]
+            gr1=[4]
+            ch1=[0,0]
+            ms1=[0]
+            gr2=[0]
+            ms2=[1,11]
+            mpi_gather_recv gr1 , ms1 , ch1
+            ms2[1]=ms1[0]*coef[3]
+            mpi_gather_send gr2 , ms2
+          }
+        },
+
+        proc{
+          7.times{|i|
+            coef=[1,2,1,2,1,2,3,2,3,2]
+            gr1=[4]
+            ch1=[0,0]
+            ms1=[0]
+            gr2=[0]
+            ms2=[2,12]
+            mpi_gather_recv gr1 , ms1 , ch1
+            ms2[1]=ms1[0]*coef[5]
+            mpi_gather_send gr2 , ms2
+          }
+        },
+
+        proc{
+          7.times{|i|
+            gr1=[1,2,3]
+            ms1=[0,32]
+            val=[4,2,3,8,4,6,1]
+            ms1[1]=val[i]
+            mpi_gather_send gr1 , ms1
+          }
+        }
+
+      ]
+
+    }.should output_to_fd(
+
+      "[56]\n"+
+      "[28]\n"+
+      "[42]\n"+
+      "[112]\n"+
+      "[56]\n"+
+      "[84]\n"+
+      "[14]\n",
+
+    STDOUT)
+
+  end
+
 end
 
