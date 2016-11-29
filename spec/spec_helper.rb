6a112b87b129217b82dd7aadd9afe9f18a00238d
macbookpr02010 <github@gianduia.net>
2016-11-29 18:22:36 +0100

    Changed codeclimate-test-reporter with simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 if ENV.has_key? "CODECLIMATE_REPO_TOKEN"
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
+  #require "codeclimate-test-reporter"
+  #CodeClimate::TestReporter.start
+  require "simplecov"
+  SimpleCov.start
 end
 
 require "rubygems"
